5646ff0bbc6df24e424438a8450bb6baf0d25422
IraKolbasina <irenLev@yandex.ru>
2023-08-19 09:25:33 +0300

    first commit

--- src/test/java/ru/netology/web/DeliveryCardTets.java
+++ src/test/java/ru/netology/web/DeliveryCardTets.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCardTets {
@@ -20,16 +18,22 @@ public class DeliveryCardTets {
     }
 
     @Test
-    public void shouldRegisterDeliveryCadr() {
+    public void DeliveryCardTests() {
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Москва");
-        String currentDate = generateDate(4, "dd.mm.yyyy");
+        $("[data-test-id='city'] input").setValue("Казань");
+
+        String currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
-        $("[data-test-id='name'] input").setValue("Петров Леондид");
+
+        $("[data-test-id='name'] input").setValue("Петров Иван");
         $("[data-test-id='phone'] input").setValue("+79298184425");
-        $("[data-test-id='agreement'] input").click();
+
+        $("[data-test-id='agreement']").click();
         $("button.button").click();
+
+
+
         $("notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на" + currentDate));
